f3a120656af450b0ab25db57cb32edec249508ab
Alejandro Juarez <alehendrix88@gmail.com>
2026-08-06 18:13:50 -0600

    Primer modelo para canciones

---
+++ main.py
@@ -0,0 +1,10 @@
+canciones = []
+cancion1 = {'titulo':'Enter Sandman', 'artista':'Metallica', 'duracion_segundos':331, 'afinacion':'E estandar', 'dificultad':'media', 'youtube':'https://youtu.be/CD-E-LDc384?si=9DZrQfS-zpgabWtv', 'tablatura':'https://youtu.be/NIAkEJp8i_Y?si=XB4Vp_HMRYBpN8TM' }
+canciones.append(cancion1)
+if canciones == []:
+    print("No hay canciones todavia")
+else:
+    print("ya hay canciones")
+    print(canciones)
+
+   
